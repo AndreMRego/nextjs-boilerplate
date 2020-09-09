3c369b245be61222bea7256603d0ca64b3fea23c
Andre Rego <andremregodev@gmail.com>
2020-09-09 18:46:38 -0300

    refactor: config typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+type Props = {
+  title: string
+}
+
+export default function Home({ title = 'React Avançado' }:Props) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">{title}!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
