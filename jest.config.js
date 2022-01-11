c4dc2941d12d5f756ae7625ce859d8d20fa0bc0b
Andre Rego <andremregodev@gmail.com>
2022-01-11 18:39:50 -0300

    update to use SWC instead of Babel

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({ dir: '.' });
+
+const customJestConfig = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
@@ -9,4 +15,9 @@ module.exports = {
       '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
   },
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
 };
+
+module.exports = createJestConfig(customJestConfig);
