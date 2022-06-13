5bd137821eb27e730531dc9c57399bc267e10ad6
Andre Rego <idd_arego@uolinc.com>
2022-06-13 19:14:58 -0300

    chore: update project to react 18

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,11 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const nextJest = require('next/jest');
-
-const createJestConfig = nextJest({ dir: '.' });
-
-const customJestConfig = {
+module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  moduleNameMapper: {
-    '^styled-components':
-      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
-  },
-  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  modulePaths: ['<rootDir>/src/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-  },
-};
-
-module.exports = createJestConfig(customJestConfig);
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
+}
